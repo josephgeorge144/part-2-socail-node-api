4b132880d5122a113189e30a69119b8fe35de7d8
Joseph George Venmelil <84468450+josephgeorge144@users.noreply.github.com>
2022-11-01 08:07:40 +0000

    users follow and unfollow routers added

--- routes/users.js
+++ routes/users.js
@@ -1,94 +1,108 @@
-const router=require('express').Router();
+const router = require("express").Router();
 const User = require("../models/User");
-const bcrypt=require('bcrypt');
-const { error } = require('console');
+const bcrypt = require("bcrypt");
+const { error } = require("console");
 
 //update user
-router.put('/:id',async(req,res)=>{
-    if(req.body.userId===req.params.id || req.body.isAdmin){
-       if(req.body.password){
-        try{
-            const salt=await bcrypt.genSalt(10)
-            req.body.password=await bcrypt.hash(req.body.password,salt)
-        }
-        catch(err){
-            console.log(err);
-            return res.status(500).json(err)
-           
-        }
-       }
-       try{
-        const user=await User.findByIdAndUpdate(req.params.id,{$set:req.body});  
-         res.status(200).json("Account has been updated")
+router.put("/:id", async (req, res) => {
+  if (req.body.userId === req.params.id || req.body.isAdmin) {
+    if (req.body.password) {
+      try {
+        const salt = await bcrypt.genSalt(10);
+        req.body.password = await bcrypt.hash(req.body.password, salt);
+      } catch (err) {
+        console.log(err);
+        return res.status(500).json(err);
+      }
     }
-    catch(err){
-       return res.status(500).json(err)
+    try {
+      const user = await User.findByIdAndUpdate(req.params.id, {
+        $set: req.body,
+      });
+      res.status(200).json("Account has been updated");
+    } catch (err) {
+      return res.status(500).json(err);
     }
+  } else {
+    console.log(req.body.userId);
+    console.log(req.params.id);
 
-    }
+    return res.status(403).json("you can updqate only your account");
+  }
 
-    else{
-        console.log(req.body.userId);
-        console.log(req.params.id);
+  res.send("welocme new");
+});
 
-        return res.status(403).json('you can updqate only your account')
-
-    }
-
-    res.send('welocme new')})
-
-    
 //delete user
-    router.delete('/:id',async(req,res)=>{
-        if(req.body.userId===req.params.id || req.body.isAdmin){
-           
-           try{
-               await User.findByIdAndDelete(req.params.id);  
-             return res.status(200).json("Account has been deleted successfully")
-        }
-        catch(err){
+router.delete("/:id", async (req, res) => {
+  if (req.body.userId === req.params.id || req.body.isAdmin) {
+    try {
+      await User.findByIdAndDelete(req.params.id);
+      return res.status(200).json("Account has been deleted successfully");
+    } catch (err) {
+      console.log(req.body.userId);
+      console.log(req.params.id);
 
-            console.log(req.body.userId);
-            console.log(req.params.id);
-            
-           return res.status(500).json(err)
-        }
-    
-        }
-    
-        else{
-           
-    
-            return res.status(403).json('you can delete only your account')
-    
-        }
-    
-        })
+      return res.status(500).json(err);
+    }
+  } else {
+    return res.status(403).json("you can delete only your account");
+  }
+});
 
-        //getuserr
-        router.get('/:id',async (req,res)=>{
-            try{
-                const user =await User.findById(req.params.id);
-                const{password,updatedAt,...other}=user._doc
-                // user._doc is the returned user object from collection
-                return res.status(200).json(other)
-            }
-            catch(err){
-                res.status(500).json('kityilla')
-            }
-        })  
-        
-        //follow user
-        router.put("id/:id/follow",async(req,res)=>{
-            if(req.body.userId !== req.params.id){
-                try{
+//getuserr
+router.get("/:id", async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    const { password, updatedAt, ...other } = user._doc;
+    // user._doc is the returned user object from collection
+    return res.status(200).json(other);
+  } catch (err) {
+    res.status(500).json("kityilla");
+  }
+});
 
-                    const user=await user.findById(req.params.id);
-                    const currentUser=await user.findById(req.body.userId);
-                }
-                catch(err){
-                    res.status(500).json(err)
-                }
-            }
-        })
-module.exports=router
+//follow user
+router.put("/:id/follow", async (req, res) => {
+  if (req.body.userId !== req.params.id) {
+    try {
+      const user = await User.findById(req.params.id);
+      const currentUser = await User.findById(req.body.userId);
+      if (!user.followers.includes(req.body.userId)) {
+        await user.updateOne({ $push: { followers: req.body.userId } });
+        await currentUser.updateOne({ $push: { followins: req.params.id } });
+        res.status(200).json("user has been followed");
+      } else {
+        res.status(403).json("you are already following this person");
+      }
+    } catch (err) {
+      res.status(500).json("sf");
+    }
+  } else {
+    res.status(403).json("you cant follow yourself");
+  }
+});
+
+//unfollow
+
+router.put("/:id/unfollow", async (req, res) => {
+    if (req.body.userId !== req.params.id) {
+      try {
+        const user = await User.findById(req.params.id);
+        const currentUser = await User.findById(req.body.userId);
+        if (!user.followers.includes(req.body.userId)) {
+          await user.updateOne({ $pull: { followers: req.body.userId } });
+          await currentUser.updateOne({ $pull: { followins: req.params.id } });
+          res.status(200).json("user has been followed");
+        } else {
+          res.status(403).json("you are already following this person");
+        }
+      } catch (err) {
+        res.status(500).json("sf");
+      }
+    } else {
+      res.status(403).json("you cant follow yourself");
+    }
+  });
+
+module.exports = router;
